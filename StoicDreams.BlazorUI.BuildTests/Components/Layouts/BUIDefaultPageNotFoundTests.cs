e0adb5e115cbbaf7c7cd2d15a844c578db98382e
Erik Gassler <erikgassler@live.com>
2022-07-21 07:47:33 -0700

    some tests and minor content tweaks

---
+++ StoicDreams.BlazorUI.BuildTests/Components/Layouts/BUIDefaultPageNotFoundTests.cs
@@ -0,0 +1,17 @@
+﻿namespace StoicDreams.BlazorUI.Components.Layouts;
+
+public class BUIDefaultPageNotFoundTests : TestFrameworkBlazor
+{
+	[Fact]
+	public void Verify_Render()
+	{
+		IRenderActions<BUIDefaultPageNotFound> actions = ArrangeRenderTest<BUIDefaultPageNotFound>(_ => { }, this.StartupTestServices);
+
+		actions.Act(a => { });
+
+		actions.Assert(a =>
+		{
+			a.Render.Markup.Should().Contain("The page you are looking for could not be found");
+		});
+	}
+}

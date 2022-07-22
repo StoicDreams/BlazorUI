c7785234dc1e20b9f4cc95c67af73c3dc53c566f
Erik Gassler <erikgassler@live.com>
2022-07-22 11:00:58 -0700

    Mote test updates

---
+++ StoicDreams.BlazorUI.BuildTests/Components/Layouts/BUIPageLayoutTests.cs
@@ -0,0 +1,50 @@
+﻿using BlazorTransitionableRoute;
+using Microsoft.AspNetCore.Components;
+using System.Collections.ObjectModel;
+
+namespace StoicDreams.BlazorUI.Components.Layouts;
+
+public class BUIPageLayoutTests : TestFrameworkBlazor
+{
+	[Theory]
+	[InlineData("", true, false)]
+	public void Verify_Render_FirstRender(string expectedClass, bool intoView, bool isBackwards)
+	{
+		IRenderActions<BUIPageLayout> actions = ArrangeThisTest(intoView, isBackwards, true);
+
+		actions.Act(a => { });
+
+		actions.Assert(a =>
+		{
+			a.Render.Markup.Should().Contain("Mock Content");
+			a.Render.Find("main").ClassName.Should().BeEquivalentTo(expectedClass);
+		});
+	}
+
+	[Theory]
+	[InlineData("animate__fadeInDown", true, false)]
+	public void Verify_Render_SubsequentRenders(string expectedClass, bool intoView, bool isBackwards)
+	{
+		IRenderActions<BUIPageLayout> actions = ArrangeThisTest(intoView, isBackwards, false);
+
+		actions.Act(a => { });
+
+		actions.Assert(a =>
+		{
+			a.Render.Markup.Should().Contain("Mock Content");
+			a.Render.Find("main").ClassName.Should().Contain(expectedClass);
+		});
+	}
+
+	private IRenderActions<BUIPageLayout> ArrangeThisTest(bool intoView, bool isBackwards, bool isFirstRender)
+	{
+		return ArrangeRenderTest<BUIPageLayout>(options =>
+		{
+			Dictionary<string, object> map = new();
+			IReadOnlyDictionary<string, object> roMap = new ReadOnlyDictionary<string, object>(map);
+			RouteData routeData = new(typeof(Pages.Error), roMap);
+			options.Parameters.Add("Transition", Transition.Create(routeData, routeData, intoView, isBackwards, isFirstRender));
+			options.Parameters.Add("Body", MockRender("Mock Content"));
+		}, this.StartupTestServices);
+	}
+}

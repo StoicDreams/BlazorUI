dd55f8d0ddc8c47349d65bfb79b4692e52aabf2e
Erik Gassler <erikgassler@live.com>
2022-07-24 10:49:34 -0700

    Apply grid layout for theme manager color selections

--- StoicDreams.BlazorUI/DataTypes/ThemeConfig.cs
+++ StoicDreams.BlazorUI/DataTypes/ThemeConfig.cs
@@ -3,33 +3,51 @@
 public record ThemeConfig
 {
 	public string Name { get; set; } = "Custom";
+
 	/// <summary>
 	/// Milliseconds timing for navigation to transition pages in
 	/// Defaults to 300
 	/// </summary>
+	[Display("Nav Transition In")]
 	public int NavTransitionIn { get; set; } = 300;
+
+
 	/// <summary>
 	/// Milliseconds timing for navigation to transition pages out
 	/// </summary>
+	[Display("Nav Transition Out")]
 	public int NavTransitionOut { get; set; } = 300;
 	/// <summary>
 	/// Text / Offset color used against light backgrounds
 	/// </summary>
+	[Display("Text color for Light Backgrounds")]
 	public ColorData Black { get; set; } = "#333333";
 	/// <summary>
 	/// Text / Offset color used against dark backgrounds
 	/// </summary>
+	[Display("Text color for Dark Backgrounds")]
 	public ColorData White { get; set; } = "#ffffff";
+	[Display("Title Bar Background")]
 	public ColorData AppBarBackground { get; set; } = "#594ae2";
+	[Display("Main Body Background")]
 	public ColorData AppBackground { get; set; } = "#ffffff";
+	[Display("Drawer Background")]
 	public ColorData DrawerBackground { get; set; } = "#ffffff";
+	[Display("Primary Color")]
 	public ColorData Primary { get; set; } = "#594ae2";
+	[Display("Secondary Color")]
 	public ColorData Secondary { get; set; } = "#ff4081";
+	[Display("Tertiary Color")]
 	public ColorData Tertiary { get; set; } = "#1ec8a5";
+	[Display("Info Alerts / Classes")]
 	public ColorData Info { get; set; } = "#2196f3";
+	[Display("Success Alerts / Classes")]
 	public ColorData Success { get; set; } = "#00c853";
+	[Display("Warning Alerts / Classes")]
 	public ColorData Warning { get; set; } = "#ff9800";
+	[Display("Error Alerts / Classes")]
 	public ColorData Error { get; set; } = "#f44336";
+	[Display("Dark")]
 	public ColorData Dark { get; set; } = "#424242";
 
 	public ThemeConfig Copy => new()

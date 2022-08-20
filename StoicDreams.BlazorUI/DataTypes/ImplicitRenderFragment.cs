bb6bb2cb50b6ef460d346b430515155af49e292c
Erik Gassler <erikgassler@live.com>
2022-08-20 08:40:19 -0700

    Add ImplicitRenderFragment class for easier content creation and conversions between text and RenderFragments

---
+++ StoicDreams.BlazorUI/DataTypes/ImplicitRenderFragment.cs
@@ -0,0 +1,10 @@
+﻿namespace StoicDreams.BlazorUI.DataTypes;
+
+public class ImplicitRenderFragment
+{
+	public RenderFragment Render { get; set; } = "".ConvertToRenderFragment();
+
+	public static implicit operator ImplicitRenderFragment(string input) => new() { Render = input.ConvertToRenderFragment() };
+	public static implicit operator RenderFragment(ImplicitRenderFragment input) => input.Render;
+	public static implicit operator ImplicitRenderFragment(RenderFragment input) => new() { Render = input };
+}

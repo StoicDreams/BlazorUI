dc0ada20621c42171ac4481fd6900ce4832a20de
Erik Gassler <erikgassler@live.com>
2022-08-20 09:40:09 -0700

    Update to demonstrate various ways of mixing string, ImplicitRenderFragment, and RenderFragment segments in a list|array containing your page content

--- StoicDreams.BlazorUI/DataTypes/ImplicitRenderFragment.cs
+++ StoicDreams.BlazorUI/DataTypes/ImplicitRenderFragment.cs
@@ -4,7 +4,7 @@ public class ImplicitRenderFragment
 {
 	public RenderFragment Render { get; set; } = "".ConvertToRenderFragment();
 
-	public static implicit operator ImplicitRenderFragment(string input) => new() { Render = input.ConvertToRenderFragment() };
 	public static implicit operator RenderFragment(ImplicitRenderFragment input) => input.Render;
+	public static implicit operator ImplicitRenderFragment(string input) => new() { Render = input.ConvertToRenderFragment() };
 	public static implicit operator ImplicitRenderFragment(RenderFragment input) => new() { Render = input };
 }

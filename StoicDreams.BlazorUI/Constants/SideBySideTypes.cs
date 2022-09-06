2f4ed39818f8b437b5b3bdc0e0140f19368572fd
Erik Gassler <erikgassler@live.com>
2022-09-05 22:35:59 -0700

    add BUIImage and SideBySide components

---
+++ StoicDreams.BlazorUI/Constants/SideBySideTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace StoicDreams.BlazorUI.Constants;
+
+public enum SideBySideTypes
+{
+	/// <summary>
+	/// Left and Right are evenly split
+	/// </summary>
+	Even,
+	/// <summary>
+	/// Left fits max-content, right auto fits rest of space
+	/// </summary>
+	MaxContentAuto,
+	/// <summary>
+	/// Right fits max-content, left auto fits rest of space
+	/// </summary>
+	AutoMaxContent
+
+}

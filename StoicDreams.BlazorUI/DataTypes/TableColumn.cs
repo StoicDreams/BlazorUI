01b2884ffbcfaea384ba075bda8bf3b5cc02156a
Erik Gassler <erikgassler@live.com>
2022-09-05 19:50:11 -0700

    Added BUIStaticTable to simplify table creation of static tables

---
+++ StoicDreams.BlazorUI/DataTypes/TableColumn.cs
@@ -0,0 +1,7 @@
+﻿namespace StoicDreams.BlazorUI.DataTypes;
+
+public class TableColumn<TItem>
+{
+	public ImplicitRenderFragment Title { get; set; } = string.Empty;
+	public RenderFragment<TItem> Cell { get; set; } = null!;
+}

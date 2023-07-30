29547d7d8c7d85705c521552a6cdb77d6f5570be
Erik Gassler <erikgassler@live.com>
2023-07-30 04:09:50 -0500

    Update deprecated method argument

--- StoicDreams.BlazorUI/Data/StateManagers/AppState.cs
+++ StoicDreams.BlazorUI/Data/StateManagers/AppState.cs
@@ -2,36 +2,36 @@
 
 public class AppState : StateManager, IAppState
 {
-	public AppState(IMemoryStorage memory, IAppOptions options) : base(memory)
-	{
-	}
+    public AppState(IMemoryStorage memory, IAppOptions options) : base(memory)
+    {
+    }
 
-	public ValueTask SetDataAsync<TData>(AppStateDataTags tag, TData? data) => SetDataAsync(tag.ToString(), data);
+    public ValueTask SetDataAsync<TData>(AppStateDataTags tag, TData? data) => SetDataAsync(tag.ToString(), data);
 
-	public ValueTask<TData?> GetDataAsync<TData>(AppStateDataTags tag) => GetDataAsync<TData>(tag.ToString());
+    public ValueTask<TData?> GetDataAsync<TData>(AppStateDataTags tag) => GetDataAsync<TData>(tag.ToString());
 
-	public void SetData<TData>(AppStateDataTags tag, TData? data)
-	{
-		ValueTask task = SetDataAsync(tag.ToString(), data);
-		task.AndForget(TaskOption.None);
-	}
+    public void SetData<TData>(AppStateDataTags tag, TData? data)
+    {
+        ValueTask task = SetDataAsync(tag.ToString(), data);
+        task.AndForget(true);
+    }
 
-	public TData? GetData<TData>(AppStateDataTags tag)
-	{
-		return GetDataAsync<TData>(tag).GetAwaiter().GetResult();
-	}
+    public TData? GetData<TData>(AppStateDataTags tag)
+    {
+        return GetDataAsync<TData>(tag).GetAwaiter().GetResult();
+    }
 
-	public new void ApplyChanges(Action changeHandler)
-	{
-		base.ApplyChanges(changeHandler);
-	}
+    public new void ApplyChanges(Action changeHandler)
+    {
+        base.ApplyChanges(changeHandler);
+    }
 
-	public ValueTask TriggerChangeAsync(params AppStateDataTags[] tags)
-	{
-		ApplyChanges(() =>
-		{
-			foreach (AppStateDataTags tag in tags) { UpdatedKey(tag.ToString()); }
-		});
-		return ValueTask.CompletedTask;
-	}
+    public ValueTask TriggerChangeAsync(params AppStateDataTags[] tags)
+    {
+        ApplyChanges(() =>
+        {
+            foreach (AppStateDataTags tag in tags) { UpdatedKey(tag.ToString()); }
+        });
+        return ValueTask.CompletedTask;
+    }
 }
